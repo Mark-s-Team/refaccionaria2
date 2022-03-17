8174e4ded078341c7e21922aeb9827f327b6fda1
Marco Orta <marck1391@gmail.com>
2022-03-16 19:01:33 -0600

    Styled compoent for Title item

---
+++ src/styles/Title.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const Title = styled.div`
+span{
+    color: #fab915;
+    font-size: 16px;
+    font-weight: 700;
+    text-transform: uppercase;
+    line-height: 1;
+}
+h2{
+    margin-bottom: 20px;
+    font-size: 32px;
+    position: relative;
+    word-wrap: break-word;
+    line-height: 1;
+    font-size: 40px;
+    color: ${props => props.color};
+    margin-top: 0;
+}
+`;
