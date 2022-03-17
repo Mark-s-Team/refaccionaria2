f841f87f368c78868348bfa9b7b4935d73047d67
Marco Orta <marck1391@gmail.com>
2022-03-16 19:00:56 -0600

    Styled compoent for divider item

---
+++ src/styles/Divider.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Divider = styled.div`
+  margin-top: 25px;
+  margin-bottom: 25px;
+  width: 60px;
+  height: 2px;
+  background-color: #fab915;
+`;
