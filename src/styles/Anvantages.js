ce19ef511984aedc84d26ea64ea9b78d8d564b1e
Marco Orta <marck1391@gmail.com>
2022-03-17 18:39:31 -0600

    Refaccionaria2 page: Anvantages section styles

---
+++ src/styles/Anvantages.js
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const Anvantages = styled.div`
+    .anvantages-container {
+        padding-bottom: 75px;
+        padding-top: 100px;
+        max-width: 100%;
+        width: 1170px;
+        margin-left: auto;
+        margin-right: auto;
+        padding-left: 15px;
+        padding-right: 15px;
+    }
+    .section1 {
+        padding-left: 15px;
+        padding-right: 15px;
+    }
+    .section1-1{
+        width:50%;
+        p {
+            color: #787a7a;
+            font-weight: 300;
+            line-height: 1.625rem;
+            font-size: 16px;
+        }
+    }
+`;
